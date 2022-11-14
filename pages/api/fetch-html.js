649c0c1a9ea9876944fc3b21b4ce8693f94b8c99
dabit3 <dabit3@gmail.com>
2022-11-13 20:07:54 -0500

    removed puppeteer, added playwright

--- pages/api/fetch-html.js
+++ pages/api/fetch-html.js
@@ -1,30 +1,69 @@
 import fs from 'fs'
 import Bundlr from '@bundlr-network/client'
-import puppeteer from 'puppeteer'
+import chromium from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer-core'
+import playwright from "playwright-core";
+
+const LOCAL_CHROME_EXECUTABLE = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
 
 const PK = process.env.BNDLR_KEY
 const bundlr = new Bundlr("https://node1.bundlr.network", "matic", PK)
 
-export default async function handler(req, res) {  
+export default async function handler(req, res) {
+
+  const executablePath = await chromium.executablePath || LOCAL_CHROME_EXECUTABLE
+
   const { body } = req
   let screenshotUri = null
   console.log({ body })
   
   if (body.screenshotEnabled) {
     /* begin puppeteer */
-    const browser = await puppeteer.launch(); 
-    const page = await browser.newPage()
-    await page.goto(body.url)
-    const screenshot = await page.screenshot({ fullPage: true })
-    /* end puppeteer */
-    await browser.close()
+    // const browser = await puppeteer.launch();
 
+    const browser = await playwright.chromium.launch({
+      args: [...chromium.args, "--font-render-hinting=none"], // This way fix rendering issues with specific fonts
+      executablePath,
+      headless:
+        process.env.NODE_ENV === "production" ? chromium.headless : true,
+    })
+
+    const context = await browser.newContext()
+    const page = await context.newPage()
+
+    await page.goto(body.url, {
+      waitUntil: "load",
+    })
+    const screenshot = await page.screenshot({
+      fullPage: true
+    })
+
+    await browser.close();
     const imageTags = [{name: 'Content-Type', value: 'image/png' }]
     const imageTransaction = bundlr.createTransaction(screenshot, { tags: imageTags })
 
     await imageTransaction.sign()
     let imageId = imageTransaction.id
     await imageTransaction.upload()
+
+    // const browser = await puppeteer.launch({
+    //   executablePath,
+    //   args: [...edgeChromium.args, '--start-maximized'],
+    //   headless: false,
+    //   defaultViewport: null,
+    // })
+    // const page = await browser.newPage()
+    // await page.goto(body.url)
+    // const screenshot = await page.screenshot({ fullPage: true })
+    // /* end puppeteer */
+    // await browser.close()
+
+    // const imageTags = [{name: 'Content-Type', value: 'image/png' }]
+    // const imageTransaction = bundlr.createTransaction(screenshot, { tags: imageTags })
+
+    // await imageTransaction.sign()
+    // let imageId = imageTransaction.id
+    // await imageTransaction.upload()
     screenshotUri = `https://arweave.net/${imageId}`
   }
 

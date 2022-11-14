1ffaab4df4ed8984aa8ca990a0d500a3f722769f
dabit3 <dabit3@gmail.com>
2022-11-13 22:59:06 -0500

    added dynamic height and width

--- pages/api/fetch-html.js
+++ pages/api/fetch-html.js
@@ -5,28 +5,35 @@ const PK = process.env.BNDLR_KEY
 const bundlr = new Bundlr("https://node1.bundlr.network", "matic", PK)
 
 export default async function handler(req, res) {
-
   const { body } = req
   let screenshotUri = null
-  console.log({ body })
   
   if (body.screenshotEnabled) {
     const IS_PRODUCTION = process.env.NODE_ENV === 'production'
     let browser
     /* begin puppeteer */
-    // const browser = await puppeteer.launch();
-
     if (IS_PRODUCTION) {
-      browser = await puppeteer.connect({ browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_KEY}` })
+      browser = await puppeteer.connect({
+        browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_KEY}`,
+        defaultViewport: null
+      })
     } else {
       browser = await puppeteer.launch()
     }
 
     const page = await browser.newPage()
-    await page.goto(body.url)
-    const screenshot = await page.screenshot({ fullPage: true })
-    /* end puppeteer */
+    await page.goto(body.url, {waitUntil: 'networkidle0', timeout: 0})
+
+    const example = await page.$('html')
+    const bounding_box = await example.boundingBox()
+
+    await page.setViewport({
+      width: Math.round(bounding_box.width),
+      height: Math.round(bounding_box.height)
+    })
+    const screenshot = await page.screenshot()
     await browser.close()
+    /* end puppeteer */
 
     const imageTags = [{name: 'Content-Type', value: 'image/png' }]
     const imageTransaction = bundlr.createTransaction(screenshot, { tags: imageTags })

ae5fe749b1e8ab994b8158ba41b481f597cea2d1
dabit3 <dabit3@gmail.com>
2022-11-13 23:48:48 -0500

    updated onchange handler

--- pages/index.js
+++ pages/index.js
@@ -72,6 +72,7 @@ export default function Home() {
               setLink('')
               setHash('')
               setScreenshot('')
+              setScreenshotEnabled(false)
             }}
             placeholder="Archive a web page"
             className={inputStyle}
